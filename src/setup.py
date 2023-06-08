70c8ba55404426ab18511c458c5c622e0f800e08
Kagurazaka Ayano <128056314+ayano-kagurazaka@users.noreply.github.com>
2023-06-09 01:35:00 +0800

    Delete setup.py

--- src/setup.py
+++
@@ -1,8 +0,0 @@
-from environments import *
-from gzip import GzipFile
-from zipfile import ZipFile
-import tarfile
-from py7zr import SevenZipFile
-from rarfile import RarFile
-
-delim = "----------------------------------------"
